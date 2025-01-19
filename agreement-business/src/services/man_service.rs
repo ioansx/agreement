6d39fee18293260edaf5e51b97805c08c9d79fb5
ioansx <ioanstanilais@gmail.com>
2025-01-19 17:02:21 +0100

    display the man page nicely

--- agreement-business/src/services/man_service.rs
+++ agreement-business/src/services/man_service.rs
@@ -16,6 +16,7 @@ impl ManService {
         let pager_as_cat = "cat";
         let output = tokio::process::Command::new("man")
             .args([pager_option, pager_as_cat, &command])
+            .env("MANWIDTH", "80")
             .output()
             .await
             .map_err(|e| newer!(e, Err::internal("unable to generate man page")))?;
@@ -25,12 +26,12 @@ impl ManService {
             return Err(newer!(source, Err::internal("unable to generate man page")));
         }
 
-        let output_str = String::from_utf8(output.stdout)
+        let stdout = String::from_utf8(output.stdout)
             .map_err(|e| newer!(e, Err::internal("invalid man page stdout output")))?;
 
         Ok(ManGetOutdto {
             generated_at: DateTimeUtc::now(),
-            output: output_str,
+            output: stdout,
         })
     }
 }
--- agreement-web/static/js/page_man.js
+++ agreement-web/static/js/page_man.js
@@ -15,9 +15,10 @@ class PageMan {
     const command = document.getElementById(INPUT_COMMAND).value;
     const outdto = await this.client.get_man_page(command);
     const article = document.getElementById(ARTICLE_PAGE);
-    article.children.item(0).textContent = command;
-    article.appendChild(document.createTextNode(outdto.output));
-    article.style.display = "unset";
+    const articleHeader = article.children.item(0);
+    articleHeader.textContent = command;
+    const articlePre = article.children.item(1);
+    articlePre.innerText = outdto.output;
   };
 }
 
