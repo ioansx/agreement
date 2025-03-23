6b49b1afd17599ae8efa2aa8b649b4d88e988159
ioansx <ioanstanilais@gmail.com>
2025-03-23 15:46:13 +0100

    update FE

--- agreement-web/static/js/index.js
+++ agreement-web/static/js/index.js
@@ -1,13 +1,4 @@
 import init, { AgreementClient } from "./agreement_client.js";
-import PageMan from "./page_man.js";
-
 await init();
 
-const client = new AgreementClient(window.location.origin);
-const pageMan = new PageMan(client);
-
-window.context = {
-  pages: {
-    man: pageMan,
-  },
-};
+window.agreement = new AgreementClient(window.location.origin);
